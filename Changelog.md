96a3c7c442d05b65b4582fda871c4e3c64acfc94
adamConnerSax <adam_conner_sax@yahoo.com>
2019-09-13 13:16:54 -0400

    Initial commit.  Data sets and haskell wrapper.

---
+++ Changelog.md
@@ -0,0 +1,17 @@
+v 0.1.0.0
+* Added initial data-sets
+- election/1976-2018-house.csv
+- election/1976-2018-house_v5.csv
+- election/electionResults2018.csv
+- election/DetailedTurnoutByAgeSexRace2010-2018.csv
+- election/DetailedTurnoutByAgeSexEducation2010-2018.csv
+- campaign-finance/allSpendingThrough20181106.csv
+- campaign-finance/allSpendingThrough20180731.csv
+- campaign-finance/allSpendingFrom20180801Through20181106.csv
+- campaign-finance/forecastAndSpending.csv
+- demographic/ageSexRaceDemographics2010-2017.csv
+- demographic/ageSexEducationDemographics2010-2017.csv
+- demographic/contextDemographicsByDistrict.csv
+- dictionaries/states.csv
+- other/angryDemsContributions20181203.csv
+
---
+++ src/BlueRipple/Data/DataSourcePaths.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module BlueRipple.Data.DataSourcePaths where
+
+import qualified Paths_data_sets               as Paths
+--import qualified Frames.TH                     as F
+
+thFramesDataDir = "./data/"
+electionDir = "election/"
+demographicDir = "demographic/"
+campaignFinanceDir = "campaign-finance/"
+dictionariesDir = "dictionaries/"
+otherDir = "other/"
+
+totalSpendingCSV :: FilePath =
+  campaignFinanceDir ++ "allSpendingThrough20181106.csv"
+totalSpendingBeforeCSV :: FilePath =
+  campaignFinanceDir ++ "allSpendingThrough20180731.csv"
+totalSpendingDuringCSV :: FilePath =
+  campaignFinanceDir ++ "allSpendingFrom20180801Through20181106.csv"
+forecastAndSpendingCSV :: FilePath =
+  campaignFinanceDir ++ "forecastAndSpending.csv"
+
+houseElectionsCSV :: FilePath = electionDir ++ "1976-2018-house_v5.csv"
+detailedASRTurnoutCSV :: FilePath =
+  electionDir ++ "DetailedTurnoutByAgeSexRace2010-2018.csv"
+detailedASETurnoutCSV :: FilePath =
+  electionDir ++ "DetailedTurnoutByAgeSexEducation2010-2018.csv"
+electionResultsCSV :: FilePath = electionDir ++ "electionResult2018.csv"
+exitPoll2018CSV :: FilePath = electionDir ++ "EdisonExitPoll2018.csv"
+
+contextDemographicsCSV :: FilePath =
+  demographicDir ++ "contextDemographicsByDistrict.csv"
+ageSexRaceDemographicsLongCSV :: FilePath =
+  demographicDir ++ "ageSexRaceDemographics2010-2017.csv"
+ageSexEducationDemographicsLongCSV :: FilePath =
+  demographicDir ++ "ageSexEducationDemographics2010-2017.csv"
+
+angryDemsCSV :: FilePath = otherDir ++ "angryDemsContributions20181203.csv"
+
+
+framesPath :: FilePath -> FilePath
+framesPath x = thFramesDataDir ++ x
+
+usePath :: FilePath -> IO FilePath
+usePath x = fmap (++ x) Paths.getDataDir
+
+{-
+ccesTSV :: FilePath = dataDir ++ "CCES_cumulative_2006_2018.txt"
+
+-- the things I would make Categorical are already ints. :(
+ccesRowGen = (F.rowGen ccesTSV) { F.tablePrefix = "CCES"
+                                , F.separator   = "\t"
+                                , F.rowTypeName = "CCES"
+                                }
+-}
+
+{-
+turnoutCSV :: FilePath = dataDir ++ "Turnout2012-2018.csv"
+identityDemographics2012CSV :: FilePath =
+  dataDir ++ "identityDemographicsByDistrict2012.csv"
+identityDemographics2014CSV :: FilePath =
+  dataDir ++ "identityDemographicsByDistrict2014.csv"
+identityDemographics2016CSV :: FilePath =
+  dataDir ++ "identityDemographicsByDistrict2016.csv"
+identityDemographics2017CSV :: FilePath =
+  dataDir ++ "identityDemographicsByDistrict2017.csv"
+-}
