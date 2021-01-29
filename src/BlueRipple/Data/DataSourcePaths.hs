9ad48f64fc488daf86d85bf20234946c3a26a177
adamConnerSax <adam_conner_sax@yahoo.com>
2021-01-29 17:26:52 -0500

    Fixed data error in Senate election result dataset

--- src/BlueRipple/Data/DataSourcePaths.hs
+++ src/BlueRipple/Data/DataSourcePaths.hs
@@ -43,7 +43,7 @@ houseElectionsCSV :: FilePath
 houseElectionsCSV = electionDir ++ "1976-2018-house_v5_u1.csv"
 
 senateElectionsCSV :: FilePath
-senateElectionsCSV = electionDir ++ "1976-2020-senate.csv"
+senateElectionsCSV = electionDir ++ "1976-2020-senate_u1.csv"
 
 allMoney2020CSV :: FilePath
 allMoney2020CSV = campaignFinanceDir ++ "allMoney_20200902.csv"
